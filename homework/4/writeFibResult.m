a56058da65916f53e74ed1af20ce5e5be530d4af
Henry Rossiter <henry.rossiter@utexas.edu>
2017-11-10 00:37:23 -0600

    finished a few more problems

---
+++ homework/4/writeFibResult.m
@@ -0,0 +1,20 @@
+output = fopen('fibOutput.csv', 'w') ;
+fprintf(output, '%s,', 'n', 'fib') ;
+fprintf(output, '%s\n', 'runtime') ;
+fclose(output) ;
+for i = 1:35
+    s = timeFib(i);
+    m = {i, s.fib, s.runtime}
+    dlmwrite('fibOutput.csv', m,'newline', 'pc', '-append') ;
+end
+
+output = fopen('fibLoopOutput.csv', 'w') ;
+fprintf(output, '%s,', 'n', 'fib') ;
+fprintf(output, '%s,\n', 'runtime') ;
+fclose(output) ;
+for i = 1:35
+    s = timeFibLoop(i);
+    m = {i, s.fib, s.runtime}
+    dlmwrite('fibLoopOutput.csv', m,'newline', 'pc', '-append') ;
+end
+ 
