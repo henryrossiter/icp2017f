f3555d696bdf1ae3e56801f588cf6917109d5edc
Henry Rossiter <henry.rossiter@utexas.edu>
2017-11-12 22:25:06 -0600

    deleting .asv file backups

--- homework/4/writeFibResult.m
+++ homework/4/writeFibResult.m
@@ -4,7 +4,7 @@ fprintf(output, '%s\n', 'runtime') ;
 fclose(output) ;
 for i = 1:35
     s = timeFib(i);
-    m = {i, s.fib, s.runtime}
+    m = {i, s.fib, s.runtime};
     dlmwrite('fibOutput.csv', m,'newline', 'pc', '-append') ;
 end
 
@@ -14,7 +14,7 @@ fprintf(output, '%s,\n', 'runtime') ;
 fclose(output) ;
 for i = 1:35
     s = timeFibLoop(i);
-    m = {i, s.fib, s.runtime}
+    m = {i, s.fib, s.runtime};
     dlmwrite('fibLoopOutput.csv', m,'newline', 'pc', '-append') ;
 end
  
